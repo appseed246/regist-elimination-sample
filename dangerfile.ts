e359d6d602058e731c299bb7b2394a214cd2ea78
appseed246 <guitar.telecaster2017@gmail.com>
2023-12-21 22:00:14 +0900

    追加

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,4 @@
-import { danger, warn } from "danger";
+import { danger, markdown, warn } from "danger";
 
 const findRegist = (content: string) => {
   return content.match(/regist/)
@@ -10,7 +10,16 @@ const warnRegistInPullRequest = () => {
   const body = pullRequest.body
 
   if (findRegist(title) || findRegist(body)) {
-    warn(`## regist発見！！！
+    warn("⚠️👮🏻‍♂️⚠️👮🏻‍♂️⚠️regist警察出動！！！⚠️👮🏻‍♂️⚠️👮🏻‍♂️⚠️👮🏻")
+    markdown(`
+## ちょっとまってください！
+もしかして、登録するという意味で『regist』という単語をソースコード内で使用しようとしていませんか？？
+『regist』という単語は存在しません！！
+「登録する」という意味の英単語は『『『register』』』です！！！！気をつけるように！！！
+
+### 参考資料
++ [ソースコードの頻出単語 regist (覚えるべからず) | MSeeeeN](https://mseeeen.msen.jp/resist-regist/)
++ 
 `)
   }
 }
