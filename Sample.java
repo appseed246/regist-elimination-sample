e948a4f34693042f9024c46b68cf5e139363079d
appseed246 <guitar.telecaster2017@gmail.com>
2023-12-22 02:57:45 +0900

    checkstyleのルール変更

--- Sample.java
+++ Sample.java
@@ -1,6 +1,7 @@
 public class Sample {
     public static void regist() {
         var sample = "regist is great!";
+        var PRE_REGIST_VALUE = "sample";
         System.out.println(sample);
     }
 }
